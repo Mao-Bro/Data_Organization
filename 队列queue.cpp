a772d2e0b8a938c13b32caacd8075cfeeacb73d9
Mao-Bro <130661793+Mao-Bro@users.noreply.github.com>
2023-10-18 22:51:48 +0800

    Update 队列queue.cpp

--- 队列queue.cpp
+++ 队列queue.cpp
@@ -1,175 +1,175 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//struct Node {
-//	int val;
-//	struct Node* next;
-//	Node(): val(0), next(nullptr) { }
-//	Node(int _val): val(_val), next(nullptr) { }
-//};
-//
-//class link_queue {
-//public:
-//	link_queue() {
-//		this->_size = 0;
-//		this->front = new Node();
-//		this->rear = this->front;
-//	}
-//
-//	~link_queue() {
-//		struct Node* pre = this->front;
-//		struct Node* ptr = this->front->next;
-//
-//		while (ptr) {
-//			delete(pre);
-//			pre = ptr;
-//			ptr = ptr->next;
-//		}
-//		delete(pre);
-//
-//		ptr = nullptr;
-//		pre = nullptr;
-//
-//		this->front = nullptr;
-//		this->rear = nullptr;
-//	}
-//
-//	int size() {
-//		return this->_size;
-//	}
-//
-//	bool empty() {
-//		return this->front == this->rear;
-//	}
-//
-//	int peek() {
-//		if (!this->empty()) return this->front->next->val;
-//		else cout << "¶ÓÁĞÎª¿Õ" << endl;
-//	}
-//
-//	void clear() {
-//		this->~link_queue();
-//		this->_size = 0;
-//		this->front = new Node();
-//		this->rear = this->front;
-//	}
-//
-//	void push(int _val) {
-//		struct Node* newNode = new Node(_val);
-//		this->rear->next = newNode;
-//		this->rear = newNode;
-//		newNode = nullptr;
-//		this->_size++;
-//	}
-//
-//	void pop() {
-//		if (this->empty()) cout << "¶ÓÁĞÎª¿Õ" << endl;
-//		else {
-//			struct Node* ptr = this->front->next;
-//			this->front->next = ptr->next;
-//			ptr->next = nullptr;
-//			delete(ptr);
-//			ptr = nullptr;
-//			this->_size--;
-//		}
-//	}
-//private:
-//	struct Node* front;
-//	struct Node* rear;
-//	int _size;
-//};
-//
-//class linear_queue {
-//public:
-//	linear_queue() {
-//		this->q = (int*)malloc(2 * sizeof(int));	//¶ÓÁĞ³õÊ¼ÈİÁ¿ÉèÖÃÎª2£¬²ÉÓÃ¼Ó±¶À©Èİ²ßÂÔ£»
-//		this->maxsize = 2;
-//
-//		this->front = 0;
-//		this->rear = this->front;
-//		this->count = 0;
-//	}
-//
-//	~linear_queue() {
-//		delete[] this->q;
-//	}
-//
-//	int size() {
-//		return this->count;
-//	}
-//
-//	bool full() {
-//		return this->count == this->maxsize;
-//	}
-//
-//	void push(int _val) {
-//		if (this->full()) {			//µ±Ç°¶ÓÁĞÒÑ¾­´ïµ½×î´óÈİÁ¿£¬ÎÒÃÇĞèÒªÏÈÀ©ÈİÔÙÈë¶ÓĞÂÔªËØ£»
-//			int* newMem = (int*)malloc(2 * this->maxsize * sizeof(int));
-//			this->maxsize = this->maxsize * 2;
-//
-//			int i = 0;
-//			int j = this->front;
-//			int cont = this->maxsize / 2;
-//			while (cont--) {
-//				newMem[i++] = this->q[(j++) % (this->maxsize / 2)];
-//			}
-//
-//			this->q = newMem;
-//			newMem = nullptr;
-//
-//			this->front = 0;
-//			this->rear = this->maxsize / 2;
-//			this->count = this->maxsize / 2;
-//
-//			this->q[this->rear] = _val;
-//			this->rear++;
-//			this->count++;
-//		}
-//		else {						//µ±Ç°¶ÓÁĞ»¹Î´´ïµ½×î´óÈİÁ¿£¬ÎÒÃÇ¿ÉÒÔÖ±½ÓÈë¶ÓĞÂÔªËØ£»
-//			this->q[this->rear] = _val;
-//			this->rear = (this->rear + 1) % maxsize;
-//			this->count++;
-//		}
-//	}
-//
-//	bool empty() {
-//		return this->count == 0;
-//	}
-//
-//	void pop() {
-//		if (this->empty()) cout << "µ±Ç°¶ÓÁĞÎª¿Õ" << endl;
-//		else {
-//			this->front = (this->front + 1) % this->maxsize;
-//			this->count--;
-//		}
-//	}
-//
-//	int peek() {
-//		if (this->empty()) cout << "µ±Ç°¶ÓÁĞÎª¿Õ" << endl;
-//		return this->q[this->front];
-//	}
-//
-//	void clear() {
-//		this->~linear_queue();
-//
-//		this->q = (int*)malloc(2 * sizeof(int));
-//		this->maxsize = 2;
-//
-//		this->front = 0;
-//		this->rear = this->front;
-//		this->count = 0;
-//	}
-//private:
-//	int front;
-//	int rear;
-//	int count;
-//	int maxsize;
-//	int* q;
-//};
-//
-//int main(void) {
-//	/* ÔÚ´Ë´¦Ìí¼ÓÄãµÄ²âÊÔ³ÌĞò */
-//	linear_queue q = linear_queue();
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int val;
+	struct Node* next;
+	Node(): val(0), next(nullptr) { }
+	Node(int _val): val(_val), next(nullptr) { }
+};
+
+class link_queue {
+public:
+	link_queue() {
+		this->_size = 0;
+		this->front = new Node();
+		this->rear = this->front;
+	}
+
+	~link_queue() {
+		struct Node* pre = this->front;
+		struct Node* ptr = this->front->next;
+
+		while (ptr) {
+			delete(pre);
+			pre = ptr;
+			ptr = ptr->next;
+		}
+		delete(pre);
+
+		ptr = nullptr;
+		pre = nullptr;
+
+		this->front = nullptr;
+		this->rear = nullptr;
+	}
+
+	int size() {
+		return this->_size;
+	}
+
+	bool empty() {
+		return this->front == this->rear;
+	}
+
+	int peek() {
+		if (!this->empty()) return this->front->next->val;
+		else cout << "é˜Ÿåˆ—ä¸ºç©º" << endl;
+	}
+
+	void clear() {
+		this->~link_queue();
+		this->_size = 0;
+		this->front = new Node();
+		this->rear = this->front;
+	}
+
+	void push(int _val) {
+		struct Node* newNode = new Node(_val);
+		this->rear->next = newNode;
+		this->rear = newNode;
+		newNode = nullptr;
+		this->_size++;
+	}
+
+	void pop() {
+		if (this->empty()) cout << "é˜Ÿåˆ—ä¸ºç©º" << endl;
+		else {
+			struct Node* ptr = this->front->next;
+			this->front->next = ptr->next;
+			ptr->next = nullptr;
+			delete(ptr);
+			ptr = nullptr;
+			this->_size--;
+		}
+	}
+private:
+	struct Node* front;
+	struct Node* rear;
+	int _size;
+};
+
+class linear_queue {
+public:
+	linear_queue() {
+		this->q = (int*)malloc(2 * sizeof(int));	//é˜Ÿåˆ—åˆå§‹å®¹é‡è®¾ç½®ä¸º2ï¼Œé‡‡ç”¨åŠ å€æ‰©å®¹ç­–ç•¥ï¼›
+		this->maxsize = 2;
+
+		this->front = 0;
+		this->rear = this->front;
+		this->count = 0;
+	}
+
+	~linear_queue() {
+		delete[] this->q;
+	}
+
+	int size() {
+		return this->count;
+	}
+
+	bool full() {
+		return this->count == this->maxsize;
+	}
+
+	void push(int _val) {
+		if (this->full()) {			//å½“å‰é˜Ÿåˆ—å·²ç»è¾¾åˆ°æœ€å¤§å®¹é‡ï¼Œæˆ‘ä»¬éœ€è¦å…ˆæ‰©å®¹å†å…¥é˜Ÿæ–°å…ƒç´ ï¼›
+			int* newMem = (int*)malloc(2 * this->maxsize * sizeof(int));
+			this->maxsize = this->maxsize * 2;
+
+			int i = 0;
+			int j = this->front;
+			int cont = this->maxsize / 2;
+			while (cont--) {
+				newMem[i++] = this->q[(j++) % (this->maxsize / 2)];
+			}
+
+			this->q = newMem;
+			newMem = nullptr;
+
+			this->front = 0;
+			this->rear = this->maxsize / 2;
+			this->count = this->maxsize / 2;
+
+			this->q[this->rear] = _val;
+			this->rear++;
+			this->count++;
+		}
+		else {						//å½“å‰é˜Ÿåˆ—è¿˜æœªè¾¾åˆ°æœ€å¤§å®¹é‡ï¼Œæˆ‘ä»¬å¯ä»¥ç›´æ¥å…¥é˜Ÿæ–°å…ƒç´ ï¼›
+			this->q[this->rear] = _val;
+			this->rear = (this->rear + 1) % maxsize;
+			this->count++;
+		}
+	}
+
+	bool empty() {
+		return this->count == 0;
+	}
+
+	void pop() {
+		if (this->empty()) cout << "å½“å‰é˜Ÿåˆ—ä¸ºç©º" << endl;
+		else {
+			this->front = (this->front + 1) % this->maxsize;
+			this->count--;
+		}
+	}
+
+	int peek() {
+		if (this->empty()) cout << "å½“å‰é˜Ÿåˆ—ä¸ºç©º" << endl;
+		return this->q[this->front];
+	}
+
+	void clear() {
+		this->~linear_queue();
+
+		this->q = (int*)malloc(2 * sizeof(int));
+		this->maxsize = 2;
+
+		this->front = 0;
+		this->rear = this->front;
+		this->count = 0;
+	}
+private:
+	int front;
+	int rear;
+	int count;
+	int maxsize;
+	int* q;
+};
+
+int main(void) {
+	/* åœ¨æ­¤å¤„æ·»åŠ ä½ çš„æµ‹è¯•ç¨‹åº */
+	linear_queue q = linear_queue();
+	return 0;
+}
