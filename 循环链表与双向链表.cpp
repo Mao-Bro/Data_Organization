9ea7cbe0c7930892299a6531e6e7cef9d3e2f7ca
Mao-Bro <130661793+Mao-Bro@users.noreply.github.com>
2023-10-18 22:51:28 +0800

    Update 循环链表与双向链表.cpp

--- 循环链表与双向链表.cpp
+++ 循环链表与双向链表.cpp
@@ -1,205 +1,205 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//struct Node {
-//	int val;
-//	struct Node* next;
-//	Node() : val(0), next(nullptr) { }
-//	Node(int _val, struct Node* _next) : val(_val), next(_next) { }
-//};
-//
-//class circular_list {	//Ñ­»·Á´±í
-//public:
-//	circular_list() {	//¹¹Ôìº¯Êý
-//		this->head = new Node();
-//		this->head->next = this->head;
-//	}
-//
-//	~circular_list() {	//Îö¹¹º¯Êý
-//		struct Node* ptr = this->head;
-//		while (ptr->next != ptr) {
-//			struct Node* node = ptr->next;
-//			ptr->next = node->next;
-//
-//			node->next = nullptr;
-//			delete(node);
-//			node = nullptr;
-//		}
-//		ptr = nullptr;
-//		delete(this->head);
-//		this->head = nullptr;
-//	}
-//
-//	bool empty() {		//ÅÐ¿Õº¯Êý
-//		return this->head->next == this->head;
-//	}
-//
-//	void insert_head(int _val) {	//Í·²å·¨²åÈëÐÂ½Úµã
-//		struct Node* newNode = new Node(_val, this->head->next);
-//		this->head->next = newNode;
-//		newNode = nullptr;
-//	}
-//
-//	void insert_tail(int _val) {	//Î²²å·¨²åÈëÐÂ½Úµã
-//		struct Node* pre_head = this->head;
-//		while (pre_head->next != this->head) {
-//			pre_head = pre_head->next;
-//		}
-//
-//		struct Node* newNode = new Node(_val, this->head);
-//		pre_head->next = newNode;
-//
-//		pre_head = nullptr;
-//		newNode = nullptr;
-//	}
-//
-//	int size() {	//·µ»Ø²»°üº¬ÉÚ±ø½ÚµãÔÚÄÚ£¬Ñ­»·Á´±íµ±ÖÐÓÐ¶àÉÙ¸ö½Úµã
-//		struct Node* ptr = this->head;
-//		int count = 0;
-//		while (ptr->next != this->head) {
-//			count++;
-//			ptr = ptr->next;
-//		}
-//		return count;
-//	}
-//
-//	struct Node* find(int _val) {		//Ñ°ÕÒÊýÖµÎª_valµÄ½Úµã£¬Èç¹ûÓÐ¶à¸ö½Úµã£¬Ôò·µ»ØµÚÒ»¸ö½Úµã
-//		struct Node* ptr = this->head;
-//		while (ptr->next != this->head) {
-//			if (ptr->val == _val) return ptr;
-//			ptr = ptr->next;
-//		}
-//		cout << "Ñ­»·Á´±íµ±ÖÐ²»´æÔÚÊýÖµÎª" << _val << "µÄ½Úµã" << endl;
-//		return nullptr;
-//	}
-//
-//	void print() {
-//		struct Node* ptr = this->head;
-//		cout << "µ±Ç°Á´±íµ±ÖÐ°üº¬µÄÔªËØ(°üº¬ÉÚ±ø½Úµã)Îª£º";
-//		while (ptr->next != this->head) {
-//			cout << ptr->val << " ";
-//			ptr = ptr->next;
-//		}
-//		cout << ptr->val << endl;
-//	}
-//private:
-//	struct Node* head;	//Ñ­»·Á´±íµÄÉÚ±ø½Úµã
-//};
-//
-//struct dNode {
-//	int val;
-//	struct dNode* pre;
-//	struct dNode* next;
-//	dNode() : val(0), pre(nullptr), next(nullptr) { }
-//	dNode(int _val, struct dNode* _pre, struct dNode* _next) : val(_val), pre(_pre), next(_next) { }
-//};
-//
-//class double_dir_list {
-//public:
-//	double_dir_list() {		//¹¹Ôìº¯Êý
-//		this->head = new dNode();
-//		this->tail = new dNode();
-//		
-//		this->head->next = this->tail;
-//		this->tail->pre = this->head;
-//	}
-//
-//	~double_dir_list() {	//Îö¹¹º¯Êý
-//		while (this->head->next != this->tail) {
-//			struct dNode* node = this->head->next;
-//
-//			this->head->next = node->next;
-//			node->next->pre = node->pre;
-//			node->next = nullptr;
-//			node->pre = nullptr;
-//
-//			delete(node);
-//			node = nullptr;
-//		}
-//
-//		this->head->next = nullptr;
-//		this->tail->pre = nullptr;
-//
-//		delete(this->head);
-//		delete(this->tail);
-//		
-//		this->head = nullptr;
-//		this->tail = nullptr;
-//	}
-//
-//	struct dNode* find(int _val) {		//·µ»ØµÚÒ»¸öÊýÖµÎª_valµÄ½ÚµãµØÖ·
-//		struct dNode* ptr = this->head;
-//
-//		while (ptr != this->tail) {
-//			if (ptr->val == _val) return ptr;
-//			ptr = ptr->next;
-//		}
-//		cout << "Ë«ÏòÁ´±íµ±ÖÐ²»´æÔÚÊýÖµÎª" << _val << "µÄ½Úµã" << endl;
-//
-//		ptr = nullptr;
-//		return nullptr;
-//	}
-//
-//	void delete_ptr(struct dNode* ptr) {	//É¾³ýÖ¸¶¨½Úµã
-//		ptr->pre->next = ptr->next;
-//		ptr->next->pre = ptr->pre;
-//
-//		ptr->next = nullptr;
-//		ptr->pre = nullptr;
-//
-//		delete(ptr);
-//		ptr = nullptr;
-//	}
-//
-//	void delete_val(int _val) {		//É¾³ýµÚÒ»¸öÊýÖµÎª_valµÄ½Úµã
-//		struct dNode* ptr = this->find(_val);
-//		this->delete_ptr(ptr);
-//	}
-//
-//	void insert_after(struct dNode* node, int _val) {
-//		if (node == this->tail) {
-//			cout << "²»¿ÉÒÔÔÚÎ²²¿½ÚµãºóÃæ²åÈë½Úµã" << endl;
-//			return;
-//		}
-//
-//		struct dNode* newNode = new dNode(_val, node, node->next);
-//		node->next->pre = newNode;
-//		node->next = newNode;
-//
-//		node = nullptr;
-//		newNode = nullptr;
-//	}
-//
-//	void insert_before(struct dNode* node, int _val) {
-//		if (node == this->head) {
-//			cout << "²»¿ÉÒÔÔÚÍ·²¿½ÚµãÇ°Ãæ²åÈë½Úµã" << endl;
-//			return;
-//		}
-//
-//		struct dNode* newNode = new dNode(_val, node->pre, node);
-//		node->pre->next = newNode;
-//		node->pre = newNode;
-//
-//		node = nullptr;
-//		newNode = nullptr;
-//	}
-//
-//	void print() {
-//		struct dNode* ptr = this->head->next;
-//		cout << "µ±Ç°Ë«ÏòÁ´±íµ±ÖÐ°üº¬µÄÔªËØÎª£¨´ÓÍ·µ½Î²£©£º";
-//		while (ptr != this->tail) {
-//			cout << ptr->val << " ";
-//			ptr = ptr->next;
-//		}
-//		cout << endl;
-//	}
-//
-//	struct dNode* head;	//Ë«ÏòÁ´±íµÄÉÚ±øÍ·½Úµã
-//	struct dNode* tail; //Ë«ÏòÁ´±íµÄÉÚ±øÎ²½Úµã
-//};
-//
-//int main(void) {
-//	/* ÔÚ´Ë´¦Ìí¼Ó²âÊÔ³ÌÐò */
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int val;
+	struct Node* next;
+	Node() : val(0), next(nullptr) { }
+	Node(int _val, struct Node* _next) : val(_val), next(_next) { }
+};
+
+class circular_list {	//å¾ªçŽ¯é“¾è¡¨
+public:
+	circular_list() {	//æž„é€ å‡½æ•°
+		this->head = new Node();
+		this->head->next = this->head;
+	}
+
+	~circular_list() {	//æžæž„å‡½æ•°
+		struct Node* ptr = this->head;
+		while (ptr->next != ptr) {
+			struct Node* node = ptr->next;
+			ptr->next = node->next;
+
+			node->next = nullptr;
+			delete(node);
+			node = nullptr;
+		}
+		ptr = nullptr;
+		delete(this->head);
+		this->head = nullptr;
+	}
+
+	bool empty() {		//åˆ¤ç©ºå‡½æ•°
+		return this->head->next == this->head;
+	}
+
+	void insert_head(int _val) {	//å¤´æ’æ³•æ’å…¥æ–°èŠ‚ç‚¹
+		struct Node* newNode = new Node(_val, this->head->next);
+		this->head->next = newNode;
+		newNode = nullptr;
+	}
+
+	void insert_tail(int _val) {	//å°¾æ’æ³•æ’å…¥æ–°èŠ‚ç‚¹
+		struct Node* pre_head = this->head;
+		while (pre_head->next != this->head) {
+			pre_head = pre_head->next;
+		}
+
+		struct Node* newNode = new Node(_val, this->head);
+		pre_head->next = newNode;
+
+		pre_head = nullptr;
+		newNode = nullptr;
+	}
+
+	int size() {	//è¿”å›žä¸åŒ…å«å“¨å…µèŠ‚ç‚¹åœ¨å†…ï¼Œå¾ªçŽ¯é“¾è¡¨å½“ä¸­æœ‰å¤šå°‘ä¸ªèŠ‚ç‚¹
+		struct Node* ptr = this->head;
+		int count = 0;
+		while (ptr->next != this->head) {
+			count++;
+			ptr = ptr->next;
+		}
+		return count;
+	}
+
+	struct Node* find(int _val) {		//å¯»æ‰¾æ•°å€¼ä¸º_valçš„èŠ‚ç‚¹ï¼Œå¦‚æžœæœ‰å¤šä¸ªèŠ‚ç‚¹ï¼Œåˆ™è¿”å›žç¬¬ä¸€ä¸ªèŠ‚ç‚¹
+		struct Node* ptr = this->head;
+		while (ptr->next != this->head) {
+			if (ptr->val == _val) return ptr;
+			ptr = ptr->next;
+		}
+		cout << "å¾ªçŽ¯é“¾è¡¨å½“ä¸­ä¸å­˜åœ¨æ•°å€¼ä¸º" << _val << "çš„èŠ‚ç‚¹" << endl;
+		return nullptr;
+	}
+
+	void print() {
+		struct Node* ptr = this->head;
+		cout << "å½“å‰é“¾è¡¨å½“ä¸­åŒ…å«çš„å…ƒç´ (åŒ…å«å“¨å…µèŠ‚ç‚¹)ä¸ºï¼š";
+		while (ptr->next != this->head) {
+			cout << ptr->val << " ";
+			ptr = ptr->next;
+		}
+		cout << ptr->val << endl;
+	}
+private:
+	struct Node* head;	//å¾ªçŽ¯é“¾è¡¨çš„å“¨å…µèŠ‚ç‚¹
+};
+
+struct dNode {
+	int val;
+	struct dNode* pre;
+	struct dNode* next;
+	dNode() : val(0), pre(nullptr), next(nullptr) { }
+	dNode(int _val, struct dNode* _pre, struct dNode* _next) : val(_val), pre(_pre), next(_next) { }
+};
+
+class double_dir_list {
+public:
+	double_dir_list() {		//æž„é€ å‡½æ•°
+		this->head = new dNode();
+		this->tail = new dNode();
+		
+		this->head->next = this->tail;
+		this->tail->pre = this->head;
+	}
+
+	~double_dir_list() {	//æžæž„å‡½æ•°
+		while (this->head->next != this->tail) {
+			struct dNode* node = this->head->next;
+
+			this->head->next = node->next;
+			node->next->pre = node->pre;
+			node->next = nullptr;
+			node->pre = nullptr;
+
+			delete(node);
+			node = nullptr;
+		}
+
+		this->head->next = nullptr;
+		this->tail->pre = nullptr;
+
+		delete(this->head);
+		delete(this->tail);
+		
+		this->head = nullptr;
+		this->tail = nullptr;
+	}
+
+	struct dNode* find(int _val) {		//è¿”å›žç¬¬ä¸€ä¸ªæ•°å€¼ä¸º_valçš„èŠ‚ç‚¹åœ°å€
+		struct dNode* ptr = this->head;
+
+		while (ptr != this->tail) {
+			if (ptr->val == _val) return ptr;
+			ptr = ptr->next;
+		}
+		cout << "åŒå‘é“¾è¡¨å½“ä¸­ä¸å­˜åœ¨æ•°å€¼ä¸º" << _val << "çš„èŠ‚ç‚¹" << endl;
+
+		ptr = nullptr;
+		return nullptr;
+	}
+
+	void delete_ptr(struct dNode* ptr) {	//åˆ é™¤æŒ‡å®šèŠ‚ç‚¹
+		ptr->pre->next = ptr->next;
+		ptr->next->pre = ptr->pre;
+
+		ptr->next = nullptr;
+		ptr->pre = nullptr;
+
+		delete(ptr);
+		ptr = nullptr;
+	}
+
+	void delete_val(int _val) {		//åˆ é™¤ç¬¬ä¸€ä¸ªæ•°å€¼ä¸º_valçš„èŠ‚ç‚¹
+		struct dNode* ptr = this->find(_val);
+		this->delete_ptr(ptr);
+	}
+
+	void insert_after(struct dNode* node, int _val) {
+		if (node == this->tail) {
+			cout << "ä¸å¯ä»¥åœ¨å°¾éƒ¨èŠ‚ç‚¹åŽé¢æ’å…¥èŠ‚ç‚¹" << endl;
+			return;
+		}
+
+		struct dNode* newNode = new dNode(_val, node, node->next);
+		node->next->pre = newNode;
+		node->next = newNode;
+
+		node = nullptr;
+		newNode = nullptr;
+	}
+
+	void insert_before(struct dNode* node, int _val) {
+		if (node == this->head) {
+			cout << "ä¸å¯ä»¥åœ¨å¤´éƒ¨èŠ‚ç‚¹å‰é¢æ’å…¥èŠ‚ç‚¹" << endl;
+			return;
+		}
+
+		struct dNode* newNode = new dNode(_val, node->pre, node);
+		node->pre->next = newNode;
+		node->pre = newNode;
+
+		node = nullptr;
+		newNode = nullptr;
+	}
+
+	void print() {
+		struct dNode* ptr = this->head->next;
+		cout << "å½“å‰åŒå‘é“¾è¡¨å½“ä¸­åŒ…å«çš„å…ƒç´ ä¸ºï¼ˆä»Žå¤´åˆ°å°¾ï¼‰ï¼š";
+		while (ptr != this->tail) {
+			cout << ptr->val << " ";
+			ptr = ptr->next;
+		}
+		cout << endl;
+	}
+
+	struct dNode* head;	//åŒå‘é“¾è¡¨çš„å“¨å…µå¤´èŠ‚ç‚¹
+	struct dNode* tail; //åŒå‘é“¾è¡¨çš„å“¨å…µå°¾èŠ‚ç‚¹
+};
+
+int main(void) {
+	/* åœ¨æ­¤å¤„æ·»åŠ æµ‹è¯•ç¨‹åº */
+	return 0;
+}
